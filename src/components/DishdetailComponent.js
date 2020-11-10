b55fd91f51f1f3bb106bff723dc424e890c9413a
dhanasree venkitachalam <dhanasreeiyer@gmail.com>
2020-11-10 17:13:45 -0500

    react animation components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,13 +1,11 @@
 import React,{Component} from 'react';
 import {Card, CardImg, CardTitle,CardBody,CardText, ListGroup, ListGroupItem ,BreadcrumbItem,Breadcrumb} from 'reactstrap';
 import {Link} from 'react-router-dom';
-
 import Loading from './LoadingComponent';
-
 import { Button ,Modal,ModalBody,ModalHeader,Label,Row,Col} from 'reactstrap';
 import {LocalForm,Control,Errors} from 'react-redux-form';
-
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 const required = (val) => val && val.length;
@@ -118,12 +116,14 @@ render(){
   function  RenderComments({comments,postComment,dishId}){
  
       if (comments !=null){
+         
      const comment= comments.map(
             (com)=>{
 
                 return (
+                    <Stagger in>
                 <ul key={com.id} className="list-unstyled">
-
+                    <Fade in>
             <li >{com.comment}</li>
             <li >
                 <span>--</span>
@@ -132,8 +132,9 @@ render(){
                 <span>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(com.date)))}</span>
 
             </li>
+            </Fade>
                 </ul>
-           
+                </Stagger>
                 );
 
             }
@@ -162,6 +163,12 @@ render(){
  function RenderDish({dish}){
 
     return(
+
+        <FadeTransform
+        in
+        transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
         <Card>
   <CardImg top src={baseUrl + dish.image} alt={dish.name} />
         <CardBody>
@@ -169,6 +176,7 @@ render(){
         <CardText>{dish.description}</CardText>
         </CardBody>
     </Card>
+    </FadeTransform>
 
 
     );
